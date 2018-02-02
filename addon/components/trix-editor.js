ec470a1793822b425d1bfd223dff01a7c0a5574d
Jason Mitchell <jason.mitchell.w@gmail.com>
2018-02-01 17:05:54 -0800

    fixing change callback firing too early

--- addon/components/trix-editor.js
+++ addon/components/trix-editor.js
@@ -1,17 +1,18 @@
 import Ember from 'ember';
 import layout from '../templates/components/trix-editor';
 
-const { run, computed } = Ember;
+const { set, run, computed } = Ember;
+
 export default Ember.Component.extend({
   layout,
   attachmentsDisabled: false,
   value: '',
   autofocus: false,
 
+  _value: '',
   _focused: false,
-  _value: null,
-
-  _inputId: computed('elementId',function() {
+  _initialized: false,
+  _inputId: computed('elementId', function() {
     return `trix-editor-${this.get('elementId')}`;
   }).readOnly(),
 
@@ -28,27 +29,40 @@ export default Ember.Component.extend({
   'trix-attachment-add'() {},
   'trix-attachment-remove'() {},
 
+  init() {
+    this._super();
+    this._timers = [];
+  },
+
   didInsertElement() {
     this._super();
 
     let $editor = this._getEditor();
 
-    $editor.on('trix-change', run.bind(this, 'trix-change'))
-      .on('trix-initialize', run.bind(this, 'trix-initialize'))
+    $editor
+      .on('trix-change', run.bind(this, 'trix-change'))
       .on('trix-attachment-add', run.bind(this, 'trix-attachment-add'))
       .on('trix-attachment-remove', run.bind(this, 'trix-attachment-remove'))
       .on('trix-selection-change', run.bind(this, 'trix-selection-change'));
 
+    $editor.on('trix-focus', e => {
+      this._focused = true;
+      run(this, 'trix-focus', e);
+    });
+
     $editor.on('trix-blur', e => {
-      console.log('blur');
       this._focused = false;
       run(this, 'trix-blur', e);
     });
 
-    $editor.on('trix-focus', e => {
-      console.log('focus in');
-      this._focused = true;
-      run(this, 'trix-focus', e);
+    $editor.on('trix-initialize', e => {
+      run(this, 'trix-initialize', e);
+      this._timers.push(
+        run.scheduleOnce('afterRender', this, () => {
+          /* prevents doing work before it's been fully setup */
+          set(this, '_initialized', true);
+        })
+      );
     });
 
     $editor.on('trix-file-accept', e => {
@@ -58,7 +72,7 @@ export default Ember.Component.extend({
         }
 
         this['trix-file-accept'](e);
-      })
+      });
     });
   },
 
@@ -66,19 +80,24 @@ export default Ember.Component.extend({
     this._super(...arguments);
     let newValue = this.get('value');
 
-    if (this._value !== newValue && !this._focused) {
-      run.cancel(this._timer);
-      this._timer = run.schedule('afterRender', this, this._rehydrate);
+    if (
+      this._initialized &&
+      this.element &&
+      this._value !== newValue &&
+      !this._focused
+    ) {
+      this._timers.push(run.scheduleOnce('afterRender', this, this._rehydrate));
       this._value = newValue;
     }
   },
 
   willDestroyElement() {
     this._super();
-    run.cancel(this._timer);
+    this._timers.forEach(timer => run.cancel(timer));
 
     let $editor = this._getEditor();
-    $editor.off('trix-attachment-add')
+    $editor
+      .off('trix-attachment-add')
       .off('trix-attachment-remove')
       .off('trix-blur')
       .off('trix-change')
@@ -98,9 +117,6 @@ export default Ember.Component.extend({
     }
 
     let element = this._getEditor()[0];
-    let pos = element.editor.getPosition();
-    console.log('updating value', this.value);
     element.editor.loadHTML(this.value);
-    element.editor.setSelectedRange(pos);
   }
 });
